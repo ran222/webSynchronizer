adfae741e3f122b80a0130933422ff9ae1f46637
ran <xy8864@163.com>
2015-12-03 11:25:38 +0800

    init

---
+++ synchronizer/src/main/java/com/github/xy8864/synchronizer/web/action/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yuanwei
+ * Date: 2015.12.3 0003 11:23
+ * To change this template use File | Settings | File Templates.
+ */
+package com.github.xy8864.synchronizer.web.action;
